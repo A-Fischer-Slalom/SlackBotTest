611ea81fc0b744f873137de76718676e095c2c29
Glitch (express-react-slack) <none>
2021-09-02 21:48:02 +0000

    🐲🌷 Updated with Glitch

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const { App, ExpressReceiver } = require('@slack/bolt');
 const express = require("express");
 const path = require("path");
-const chatRouter = require('./routes/chatmodule');
+
+// PWAs want HTTPS!
+function checkHttps(request, response, next) {
+  // Check the protocol — if http, redirect to https.
+  if (request.get("X-Forwarded-Proto").indexOf("https") != -1) {
+    return next();
+  } else {
+    response.redirect("https://" + request.hostname + request.url);
+  }
+}
 
 // Create a Bolt Receiver
 const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
@@ -11,6 +20,37 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver
 });
+
+receiver.router.all("*", checkHttps);
+
+// A test route to make sure the server is up.
+receiver.router.get("/api/ping", (request, response) => {
+  console.log("❇️ Received GET request to /api/ping");
+  response.send("pong!");
+});
+
+receiver.router.use(express.static("public"));
+
+receiver.router.use(express.static(path.join(__dirname, 'build')));
+
+// Express port-switching logic
+let port;
+console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  port = process.env.PORT || 8080;
+  receiver.router.use(express.static(path.join(__dirname, "../build")));
+  receiver.router.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+} else {
+  port = 8080;
+  console.log("⚠️ Not seeing your changes as you develop?");
+  console.log(
+    "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
+  );
+}
+
+
 
 app.message('hello', async ({ message, say }) => {
     await say({
@@ -49,13 +89,5 @@ app.action('click_me_button', async ({ ack, body, client, say }) => {
   });
 });
 
-receiver.router.use(express.static("public"));
-
-receiver.router.use('/', chatRouter);
-
-receiver.router.use(express.static(path.join(__dirname, 'build')));
-
-(async () => {
-  await app.start(8080);
-  console.log('app is running');
-})();
+app.start(port);
+console.log('app is running');
--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pingServer = () => {
+    fetch('/api/ping')
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={pingServer}>Ping</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and your changes will live-update automatically.
         </p>
         <a
           className="App-link"
